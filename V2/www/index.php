c55d995d0e4e331f10a93c48373045d211e048fc
gigouni <nicolas.gigou@gmail.com>
2016-11-25 14:25:19 +0100

    Add the www folder

---
+++ V2/www/index.php
@@ -0,0 +1,420 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Nicolas GIGOU</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" href="favicon.ico" />
+  <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+  <link rel="stylesheet" href="assets/css/main.css" />
+  <link rel="stylesheet" href="assets/css/custom.css" />
+  <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+  <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+<body>
+
+<?php
+
+$servername = "nicolasgzostats.mysql.db";
+$username = "nicolasgzostats";
+$password = "Lasoauch4";
+$database = "nicolasgzostats";
+
+$mysqli = new mysqli($servername, $username, $password, $database);
+
+/* Vérification de la connexion */
+if ($mysqli->connect_errno) { exit(); }
+
+function clean($string) {
+  $string = html_entity_decode(preg_replace('/&([a-zA-Z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);/i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8');
+  $string = strtolower(trim(preg_replace('/[^0-9a-z]+/i', '-', $string), '-'));
+
+  return $string;
+}
+
+if(isset($_SERVER['GEOIP_CITY']) && isset($_SERVER['GEOIP_COUNTRY_NAME']) && isset($_SERVER['GEOIP_COUNTRY_CODE']) && isset($_SERVER['REMOTE_ADDR'])) 
+{
+  /* Variables pour la requête */
+  $table = "nicolasgigou_visitors";
+  $uri = $_SERVER['SCRIPT_URI'];
+  $date = new DateTime();
+  $timestamp = $date->getTimestamp();
+  $client_ip_address = clean($_SERVER['REMOTE_ADDR']);
+  $city = clean($_SERVER['GEOIP_CITY']);
+  $country = clean($_SERVER['GEOIP_COUNTRY_NAME']);
+
+  /* Insertion dans la base OVH */
+  $sql = "INSERT INTO `".$table."` VALUES (0,'".$uri."',".$timestamp.",'".$client_ip_address."','".$city."','".$country."')";
+  if ($mysqli->query($sql) === true) {} 
+  else {}
+}
+
+$mysqli->close();
+?>
+
+<!-- Sidebar -->
+<section id="sidebar">
+  <div class="inner">
+    <nav>
+      <ul>
+        <li><a href="#intro">Bienvenue</a></li>
+        <li><a href="#presentation">Présentation</a></li>
+        <li><a href="#skills">Compétences</a></li>
+        <li><a href="#projects">Projets</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</section>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+  <!-- Intro -->
+  <section id="intro" class="wrapper style1 fullscreen fade-up">
+    <div class="inner">
+      <h1>Nicolas GIGOU</h1>
+      <p>Développeur WEB.</p>
+      <ul class="actions">
+        <li><a href="#presentation" class="button scrolly">Qui suis-je ?</a></li>
+      </ul>
+    </div>
+  </section>
+
+  <!-- PRESENTATION PART -->
+  <section id="presentation" class="wrapper style2 spotlights">
+    <section>
+      <a class="image"><img src="images/coffee.jpg" alt="" data-position="center center" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Incipit</h2>
+          <p>Originaire de Brest et de son temps magnifique, 22 ans, coffee-addict et curieux du monde qui m'entoure.</p>
+          <ul class="actions">
+            <li><a href="#one_one" class="button scrolly">Et sinon ?</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="one_one">
+      <a class="image"><img src="images/student.jpg" alt="" data-position="top center" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Étudiant en M2 MIAGE</h2>
+          <p>Avant tout passionné par le monde numérique et l'écosystème des entreprises, la formation MIAGE pour
+            <i>"Méthodes Informatiques Appliquées à la Gestion des Entreprises"</i> de Rennes (35) était le parcours
+            rêvé pour atteindre mes objectifs professionnels.</p>
+          <ul class="actions">
+            <li><a href="#one_two" class="button scrolly">Mes passe-temps ?</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="one_two">
+      <a class="image"><img src="images/dev.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Passe-temps & hobbies</h2>
+          <p>Selon moi, quand on aime ce que l'on fait, ce n'est pas une corvée de travailler sur des projets parallèles
+            de développement. Ça peut aller du Pokédex se nourrissant d'une API à la mise à jour régulière de ce site sans
+            oublier l'apprentissage de nouvelles technologies comme AngularJS, NodeJS et même Android !</p>
+          <ul class="actions">
+            <li><a href="#one_three" class="button scrolly">Quelque chose à ajouter ?</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="one_three">
+      <a class="image"><img src="images/geek.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Geeky touch</h2>
+          <p>Fan de science fiction et d'histoires épiques, mon enfance a été bercée par des sagas merveilleuses telles
+            que <i>"Le Seigneur des Anneaux"</i>, <i>"Harry Potter"</i> ou encore <i>"Star Wars"</i>. Si vous aussi vous
+            partagez cette passion, je serai ravi d'en discuter avec vous autour d'une bonne bière au beurre !</p>
+          <ul class="actions">
+            <li><a href="#skills" class="button scrolly">Passons aux compétences !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </section>
+  <!-- #end PRESENTATION PART -->
+
+  <!-- SKILLS PART -->
+  <section id="skills" class="wrapper style3 fade-up">
+    <div class="inner">
+      <h2>Compétences</h2>
+      <p>Maintenant que vous avez une vision sur mon parcours académique et certains de mes projets, en quoi
+        cela est utile dans mon parcours professionel ?</p>
+      <div class="features">
+        <section>
+          <span class="icon major fa-code"></span>
+          <h3>Développement</h3>
+          <p>Quand on veut devenir un bon chef de projet, il faut savoir développer une application web en appréhendant
+            de A à Z la chaîne de production (analyse, développement, tests, ...)
+            tout en respectant les ressources humaines, techniques, financières et temporelles allouées.</p>
+        </section>
+        <section>
+          <span class="icon major fa-group"></span>
+          <h3>Gestion de projet</h3>
+          <p>Une bonne gestion de projet n'est pas initiée par un chef mais par un leader qui saura motiver son équipe et en tirer le meilleur.</p>
+        </section>
+        <section>
+          <span class="icon major fa-shopping-bag"></span>
+          <h3>Marketing</h3>
+          <p>Parce que vendre, ce n'est pas "vendre n'importe quoi à n'importe qui", il faut centrer les ventes sur les clients finaux.</p>
+        </section>
+        <section>
+          <span class="icon major fa-industry"></span>
+          <h3>Gestion de production</h3>
+          <p>Une production devrait répondre à une demande et non des clients qui répondent à une offre. Oui, mais en quelles quantités ?</p>
+        </section>
+        <section>
+          <span class="icon major fa-money"></span>
+          <h3>Comptabilité</h3>
+          <p>Une entreprise ne peut espérer rester pérenne qu'avec une gestion financière précise et efficace.</p>
+        </section>
+        <section>
+          <span class="icon major fa-rss"></span>
+          <h3>Communication</h3>
+          <p>On peut être le plus grand des génies, si on ne sait pas s'exprimer et se faire comprendre du plus grand nombre, on restera parmi les plus sots.</p>
+        </section>
+        <section>
+          <span class="icon major fa-gbp"></span>
+          <h3>Anglais</h3>
+          <p>Le monde de l'informatique est universel et évolue principalement dans une langue commune qu'il est nécessaire de maîtriser.</p>
+        </section>
+        <section>
+          <span class="icon major fa-graduation-cap"></span>
+          <h3>Autonomie</h3>
+          <p>L'autonomie, ce moteur qui nous apprend à apprendre et nous permet de répondre à des demandes avec des réponses toujours aussi pertinentes,
+            quel que soit le temps qui passe.</p>
+        </section>
+      </div>
+      <ul class="actions">
+        <li><a href="#projects" class="button scrolly">Concrêtement, en terme de projets ?</a></li>
+      </ul>
+    </div>
+  </section>
+  <!-- #end SKILLS PART -->
+
+  <!-- PROJECTS PART -->
+  <section id="projects" class="wrapper style2 spotlights">
+    <section id="projects_englishproject">
+      <a class="image"><img src="images/japan.jpg" alt="" data-position="center center" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Projet d'anglais (2nd année IUT)</h2>
+          <p>Site statique en anglais réunissant un ensemble de vidéos japonaises à caractère humoristique.</p>
+          <ul class="actions">
+            <li><a href="http://dev.nicolas-gigou.fr/embedded_projects/site_anglais/home.html" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#projects_enstabretagne" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_enstabretagne">
+      <a class="image"><img src="images/ensta.jpg" alt="" data-position="center center" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>ENSTA Bretagne</h2>
+          <p>Site internet et intranet pour le groupe REMS de l'ENSTA Bretagne.</p>
+          <ul class="actions">
+            <li><a href="http://rems.ensta-bretagne.fr/" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#projects_creperietymai" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_creperietymai">
+      <a class="image"><img src="images/tymai.jpg" alt="" data-position="top center" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Crêperie TyMaï</h2>
+          <p>Site statique présentant les produits proposés par cette crêperie de Trégastel (22).</p>
+          <ul class="actions">
+            <li><a href="http://www.creperietymai.fr/" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#projects_khenchaf" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_khenchaf">
+      <a href="#" class="image"><img src="images/office.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Page personnelle d'Ali Khenchaf</h2>
+          <p>Page personnelle de présentation du parcours du responsable du groupe REMS, M. Ali Khenchaf.</p>
+          <ul class="actions">
+            <li><a href="http://www.ensta-bretagne.fr/khenchaf/fr/index.php" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#projects_lambeconduite" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_lambeconduite">
+      <a class="image"><img src="images/lambeconduite.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>LAMBE conduite</h2>
+          <p>Site statique présentant les différentes prestations proposées par cette auto-école brestoise (29).</p>
+          <ul class="actions">
+            <li><a href="http://lambeconduite.com/" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#projects_nicolasgigou" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_nicolasgigou">
+      <a class="image"><img src="images/ng.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Nicolas Gigou</h2>
+          <p>Le site que vous êtes actuellement en train de consulter.</p>
+          <ul class="actions">
+            <li><a href="#projects_familyTree" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_familyTree">
+      <a class="image"><img src="images/familyTree_438_438.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>OneToTree</h2>
+          <p>Mon arbre généalogique avec GoJS, Bootstrap et des couleurs tendances de 2016.</p>
+          <ul class="actions">
+            <li><a href="http://dev.nicolas-gigou.fr/embedded_projects/familyTree/app/genogram.html" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#projects_vetPhoneGapCordova" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_vetPhoneGapCordova">
+      <a class="image"><img src="images/vet_438_438.png" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>PhoneGap & Cordova</h2>
+          <p>Veille technologique sur le passé, le présent et le futur de ces technologies.</p>
+          <ul class="actions">
+            <li><a href="http://nicolas-gigou.fr/assets/docs/VET.pdf" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#projects_taurus" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_taurus">
+      <a class="image"><img src="images/taurus.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Taurus (en cours de développement)</h2>
+          <p>Une application web collaborative d'aide aux étudiants par les étudiants sur les cours vus pendant les cursus de l'ISTIC.</p>
+          <ul class="actions">
+            <li><a href="http://taurus.istic.univ-rennes1.fr" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#projects_embedded" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_embedded">
+      <a class="image"><img src="images/branche.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Live previews de projets</h2>
+          <p>Certains projets ont été déployés afin de vous livrer une démonstration en direct de travaux.</p>
+          <ul class="actions">
+            <li><a href="http://dev.nicolas-gigou.fr" class="button">Montre moi !</a></li>
+            <li><a href="#projects_otherones" class="button scrolly">Next !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="projects_otherones">
+      <a class="image"><img src="images/github.jpg" alt="" data-position="25% 25%" /></a>
+      <div class="content">
+        <div class="inner">
+          <h2>Et pleins d'autres sur Github !</h2>
+          <p>Plusieurs autres projets sont disponibles sur mon compte Github, certains en privé pour les plus confidentiels.</p>
+          <ul class="actions">
+            <li><a href="https://github.com/gigouni/" target="_blank" class="button">Montre moi !</a></li>
+            <li><a href="#contact" class="button scrolly">Prenez contact !</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </section>
+  <!-- #end PROJECTS PART -->
+
+  <!-- CONTACT PART -->
+  <section id="contact" class="wrapper style1 fade-up">
+    <div class="inner">
+      <h2>Prenez contact !</h2>
+      <p>J'aime faire de nouvelles rencontres, développer mon réseau professionnel et même avoir des questions et remarques en tout genre.
+        Si vous êtes arrivés jusque là, pourquoi ne pas s'arrêter pour dire bonjour ?</p>
+      <div class="split style1">
+        <section>
+          <iframe src="//giphy.com/embed/D8SsyjjmqoUfu"
+                  width="100%"
+                  frameBorder="0"
+                  allowFullScreen>
+          </iframe>
+          <iframe src="//giphy.com/embed/RVhoFxk1GVQoE"
+                  width="100%"
+                  frameBorder="0"
+                  allowFullScreen>
+          </iframe>
+        </section>
+        <section>
+          <ul class="contact">
+            <li>
+              <h3>Adresse</h3>
+              <span>Rennes, 35200<br />
+											Bretagne, France.</span>
+            </li>
+            <li>
+              <h3>Email</h3>
+              <a href="mailto:nicolas.gigou@mail.com">nicolas.gigou@gmail.com</a>
+            </li>
+            <li>
+              <h3>Téléphone</h3>
+              <span>Envoyez moi un petit mail et je vous transmettrai mon numéro de téléphone afin de faciliter nos échanges.</span>
+            </li>
+            <li>
+              <h3>CV & réseaux pro</h3>
+              <ul class="icons">
+                <li><a href="assets/docs/CV_Nicolas_GIGOU.pdf" target="_blank" class="fa-file-pdf-o"><span class="label">Vous voulez voir mon CV ?</span></a></li>
+                <li><a href="https://github.com/gigouni/" target="_blank" class="fa-github"><span class="label">GitHub</span></a></li>
+                <li><a href="https://fr.linkedin.com/in/nicolas-gigou" target="_blank" class="fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                <li><a href="http://www.viadeo.com/p/002wvr9xdxdomu" target="_blank" class="fa-viadeo"><span class="label">Viadeo</span></a></li>
+                <li><a href="http://www.doyoubuzz.com/nicolas-gigou_1" target="_blank" class="fa-user"><span class="label">DoYouBuzz</span></a></li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  </section>
+  <!-- #end CONTACT PART -->
+
+</div>
+
+<!-- Footer -->
+<footer id="footer" class="wrapper style1-alt">
+  <div class="inner">
+    <ul class="menu">
+      <li>&copy; Nicolas GIGOU - nicolas-gigou.fr. Tous droits réservés.</li>
+    </ul>
+  </div>
+</footer>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
